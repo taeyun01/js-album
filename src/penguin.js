eda080c97f63700a63c105db10af54324487e243
taeyun <lwe12f675@gmail.com>
2024-09-18 00:13:16 +0900

    feat: 각 동물 이름 클릭 시 해당 동물의 api를 불러와 이미지 렌더링 시키기

---
+++ src/penguin.js
@@ -0,0 +1,23 @@
+const API_URL = "https://animal-api-two.vercel.app";
+
+const $content = document.querySelector("div.content");
+let template = [];
+
+const getData = async (name) => {
+  let response = await fetch(`${API_URL}/${name}`);
+  try {
+    if (response.ok) {
+      let data = await response.json();
+      data.photos.forEach((photo) => {
+        //* 먼저 template배열에 여러개 이미지 태그들을 넣어준다
+        template += `<img src=${photo.url} />`;
+      });
+      //* 그리고 나서 한번에 렌더링 시켜준다.
+      $content.innerHTML = template;
+    } else {
+      console.log("데이터를 불러오는 중 오류가 발생했습니다.");
+    }
+  } catch (err) {}
+};
+
+getData("penguin");
